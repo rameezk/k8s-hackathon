1fa93aa994f30f4d27189f2b95128f8e4849cabb
Rameez Khan <rameezkhan.sa@gmail.com>
2018-09-29 11:09:54 +0200

    Add new keys

--- infrastructure/environment/dev/outputs.tf
+++ infrastructure/environment/dev/outputs.tf
@@ -1,19 +1,3 @@
-
-output "mdm_service_bus_namespace" {
-  value = "${module.cluster.mdm_service_bus_namespace}"
-}
-output "mdm_service_bus_connection_string" {
-  value = "${module.cluster.mdm_service_bus_connection_string}"
-}
-
-output "eventhub_namespace_key" {
-  value = "${module.cluster.eventhub_namespace_key}"
-}
-
-output "eventhub_namespace" {
-  value = "${module.cluster.eventhub_namespace}"
-}
-
 output "ingress_ip" {
   value = "${module.cluster.ingress_ip}"
 }
@@ -40,8 +24,4 @@ output "resource_group" {
 
 output "cluster_name" {
   value = "${module.cluster.cluster_name}"
-}
-
-output "eventhub_connection_string" {
-  value = "${module.cluster.event_hub_connection_string}"
 }
--- infrastructure/environment/dev/terraform.tfvars
+++ infrastructure/environment/dev/terraform.tfvars
@@ -1,5 +1,5 @@
-azure_service_principal_id = "cd0b7315-af7a-407b-ac1d-762bb9e10261"
-azure_service_principal_secret = "ZlGBQssyB0sKvum4/MkFfnXvGW18xU6/WrK98rnOUzk="
+azure_service_principal_id = "4ead9366-56cf-45ae-b0bb-9c9eaf6f7791"
+azure_service_principal_secret = "s6ga3wyw4J+SPTj1hj1GEk8urhUTEUIv56/pTFyi9qE="
 azure_subscription_id = "f5ead5f6-71f4-43b3-98ab-75d14ae7a00b"
 azure_tenant_id = "9c64a0df-9e54-4428-8bae-3a56001295c7"
 
